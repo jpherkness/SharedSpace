64e52a55c53bb86f63d6af3080794ee226c0cd46
Joseph Herkness <jpherkness@gmail.com>
2015-09-29 12:15:07 -0400

    Added Firebase Implementation

--- app/src/main/java/edu/oakland/sharedspace/MainActivity.java
+++ app/src/main/java/edu/oakland/sharedspace/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import com.firebase.client.*;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+        Firebase myFirebaseRef = new Firebase("https://shared-space.firebaseio.com/");
+        myFirebaseRef.child("message").setValue("Do you have data? You'll love Firebase.");
     }
 
     @Override
