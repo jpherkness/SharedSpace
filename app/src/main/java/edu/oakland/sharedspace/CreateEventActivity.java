5db32781be9b5b185478f1a5aaebed36fc49ddca
Joseph Herkness <jpherkness@gmail.com>
2015-10-15 22:31:02 -0400

    Fixed Issue with the location EditText View OnClick handler

--- app/src/main/java/edu/oakland/sharedspace/CreateEventActivity.java
+++ app/src/main/java/edu/oakland/sharedspace/CreateEventActivity.java
@@ -5,9 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.firebase.client.Firebase;
 import com.firebase.geofire.GeoLocation;
@@ -16,7 +14,6 @@ import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 
 import java.util.Calendar;
 
@@ -24,45 +21,29 @@ public class CreateEventActivity extends AppCompatActivity{
 
     final Firebase ref = new Firebase("https://shared-space.firebaseio.com");
 
-    private static final int PLACE_PICKER_REQUEST = 1;
+    private static final int PLACE_PICKER_REQUEST_CODE = 1;
 
-    private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(
-            new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
-
-    private Button btnSignOut, btnCreateEvent;
     private EditText etTitle, etDescription, etLocation;
-    private TextView tvLocation;
 
     private LatLng eventLocation;
 
-    private Intent signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
 
-        signIn = new Intent(this, SignInActivity.class);
         etTitle = (EditText)findViewById(R.id.etTitle);
         etDescription = (EditText)findViewById(R.id.etDescription);
-
-        btnSignOut = (Button)findViewById(R.id.btnSignOut);
-        btnSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ref.unauth();
-                startActivity(signIn);
-            }
-        });
-
         etLocation = (EditText)findViewById(R.id.etLocation);
-        etLocation.setOnClickListener(new View.OnClickListener() {
+        etLocation.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
+
                 try {
                     PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
                     Intent intent = intentBuilder.build(getApplicationContext());
-                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
-
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST_CODE);
                 } catch (GooglePlayServicesRepairableException e) {
                     e.printStackTrace();
                 } catch (GooglePlayServicesNotAvailableException e) {
@@ -70,39 +51,49 @@ public class CreateEventActivity extends AppCompatActivity{
                 }
             }
         });
-
-        btnCreateEvent = (Button)findViewById(R.id.btnCreateEvent);
-        btnCreateEvent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Retrieve information about event
-                String title = etTitle.getText().toString();
-                String description = etDescription.getText().toString();
-                GeoLocation location = new GeoLocation(eventLocation.latitude, eventLocation.longitude);
-
-                //Add the event to the database
-                Event.addEvent(title, description, Calendar.getInstance().getTime(), location);
-            }
-        });
-
-
-        tvLocation = (TextView)findViewById(R.id.tvLocation);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if (requestCode == PLACE_PICKER_REQUEST && resultCode == Activity.RESULT_OK) {
+        if (requestCode == PLACE_PICKER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
 
-            // The user has selected a place. Extract the name and address.
+            // The user has selected a place. Extract the name and possibly address
             final Place place = PlacePicker.getPlace(data, this);
-
             eventLocation = place.getLatLng();
-            final CharSequence name = place.getName();
-            etLocation.setText(name);
+            etLocation.setText(place.getName());
 
         } else {
             super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void signOut(View view) {
+
+        // Un-authenticate the user
+        ref.unauth();
+
+        // Show sign in activity
+        Intent signIn = new Intent(this, SignInActivity.class);
+        startActivity(signIn);
+    }
+
+    public void createEvent(View view) {
+        // Retrieve information about event
+        String title = etTitle.getText().toString();
+        String description = etDescription.getText().toString();
+
+        // Check to make sure the event information is valid and create the event if it is
+        if(eventLocation != null && title.length() > 0){
+            GeoLocation location = new GeoLocation(eventLocation.latitude, eventLocation.longitude);
+            Event.addEvent(title, description, Calendar.getInstance().getTime(), location);
+        }else{
+            if(eventLocation == null){
+                etLocation.setError("You must enter a valid location");
+            }
+            if(title.length() == 0){
+                etTitle.setError("You must enter a valid title");
+            }
         }
     }
 }
